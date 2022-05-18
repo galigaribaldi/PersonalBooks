a6cd563cbde4535707ad5d357957ba0d313fb5c9
galigaribaldi <hcabrera@e-bitware.com.mx>
2022-05-17 19:36:55 -0500

    Add first charge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #from data.connection import Connection
 from Data.connection import Connection
 from Data.parser import Parser
-con = Connection()
-c = con.Select(tableName="books", nameRows=("title", "author"))
-print(c)
+#con = Connection()
+#c = con.Select(tableName="books", nameRows=("title", "author"))
+#print(c)
 par = Parser()
 p = par.ReadData('Books')
